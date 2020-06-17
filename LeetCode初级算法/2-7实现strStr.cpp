16a8302a1d7dada0fe3b8043c8dbc81e84431dda
jianwu585218 <1449214372@qq.com>
2020-06-17 21:35:59 +0800

    测试指针

--- LeetCode初级算法/2-7实现strStr.cpp
+++ LeetCode初级算法/2-7实现strStr.cpp
@@ -1,21 +1,22 @@
 #include"leetcode.h"
 
 
+int * test(int *x)
+{
+	*x = 2;
+	cout << *x << endl;
+	return x;
+}
+
 int main()
 {
 
-	//Solution s;
-	vector<int> v;
-	vector<int> v_next;
-	v.push_back(1);
-	v.push_back(2);
-	string res;
-	
-	for (int i = 0; i<v.size(); i++)
-	{
-		res = res.append( to_string(v[i]));
-	}
-	cout << res << endl;
+	int m = 1;
+	int* x = test(&m);
+
+	cout << *x << endl;
+	cout << m << endl;
+
 	system("pause");
 	return 0;
 }
