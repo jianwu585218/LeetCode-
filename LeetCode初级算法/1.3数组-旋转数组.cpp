64b54dc2aaf528ce170e170b843db2e0262e5fc8
jianwu585218 <1449214372@qq.com>
2020-06-17 17:07:13 +0800

    取消注释

--- LeetCode初级算法/1.3数组-旋转数组.cpp
+++ LeetCode初级算法/1.3数组-旋转数组.cpp
@@ -1,20 +1,21 @@
-///*给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。*/
-//
-//class Solution {
-//public:
-//	void rotate(vector<int>& nums, int k) {
-//
-//
-//		//左移nums.size()-k位=右移k位
-//		for (int i = 0; i<nums.size() - (k%nums.size()); i++)
-//		{
-//			int front = nums.front();
-//			nums.erase(nums.begin());
-//
-//			nums.push_back(front);
-//
-//		}
-//
-//
-//	}
-//};
+#include"leetcode.h"
+/*给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。*/
+
+class Solution1_3 {
+public:
+	void rotate(vector<int>& nums, int k) {
+
+
+		//左移nums.size()-k位=右移k位
+		for (int i = 0; i<nums.size() - (k%nums.size()); i++)
+		{
+			int front = nums.front();
+			nums.erase(nums.begin());
+
+			nums.push_back(front);
+
+		}
+
+
+	}
+};
